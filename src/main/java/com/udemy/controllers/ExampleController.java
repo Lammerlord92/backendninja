c137742fc0e2667629d62dafe261db622fa7c911
Lammerlord92 <javcarfer92@gmail.com>
2017-02-20 16:52:38 +0100

    Listado en la plantilla (Tableada)

--- src/main/java/com/udemy/controllers/ExampleController.java
+++ src/main/java/com/udemy/controllers/ExampleController.java
@@ -1,5 +1,8 @@
 package com.udemy.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,14 +21,25 @@ public class ExampleController {
 	//@GetMapping("/exampleString") hace lo mismo que la de abajo, se usa para no repetir
 	@RequestMapping(value="/exampleString", method=RequestMethod.GET)
 	public String exampleString(Model model){
-		model.addAttribute("person", new Person("John", 23));
+		model.addAttribute("people", getPeople());
 		return EXAMPLE_VIEW;
 	}
 	//Segunda forma
 	@RequestMapping(value="/exampleMAV", method=RequestMethod.GET)
 	public ModelAndView exampleMAV(){
 		ModelAndView mav=new ModelAndView(EXAMPLE_VIEW);
-		mav.addObject("person", new Person("Mike", 30));
+		mav.addObject("people", getPeople());
 		return mav;
+	}
+	
+	private List<Person> getPeople(){
+		List<Person> people= new ArrayList<Person>();
+		
+		people.add(new Person("John", 23));
+		people.add(new Person("Mike", 30));
+		people.add(new Person("Eva", 40));
+		people.add(new Person("Peter", 19));
+		
+		return people;
 	}
 }
