18263b7ac5ea4a34ba79573765c7aa7745355011
Lammerlord92 <javcarfer92@gmail.com>
2017-02-20 16:37:00 +0100

    Añadida variable en vista con Thymeleaf

--- src/main/java/com/udemy/controllers/ExampleController.java
+++ src/main/java/com/udemy/controllers/ExampleController.java
@@ -1,6 +1,7 @@
 package com.udemy.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,12 +15,15 @@ public class ExampleController {
 	//Primera forma
 	//@GetMapping("/exampleString") hace lo mismo que la de abajo, se usa para no repetir
 	@RequestMapping(value="/exampleString", method=RequestMethod.GET)
-	public String exampleString(){
+	public String exampleString(Model model){
+		model.addAttribute("name", "Jon");
 		return EXAMPLE_VIEW;
 	}
 	//Segunda forma
 	@RequestMapping(value="/exampleMAV", method=RequestMethod.GET)
 	public ModelAndView exampleMAV(){
-		return new ModelAndView(EXAMPLE_VIEW);
+		ModelAndView mav=new ModelAndView(EXAMPLE_VIEW);
+		mav.addObject("name", "Mike");
+		return mav;
 	}
 }
