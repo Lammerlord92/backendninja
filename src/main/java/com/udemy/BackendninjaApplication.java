0f67924a3c495e8c1fd97bd1064ecedfb96350a2
Lammerlord92 <javcarfer92@gmail.com>
2017-02-20 11:29:36 +0100

    Añadidas plantillas de páginas de login y errores, css e imágenes.

--- src/main/java/com/udemy/BackendninjaApplication.java
+++ src/main/java/com/udemy/BackendninjaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendninjaApplication {
-
+//Arranca la app
 	public static void main(String[] args) {
 		SpringApplication.run(BackendninjaApplication.class, args);
 	}
