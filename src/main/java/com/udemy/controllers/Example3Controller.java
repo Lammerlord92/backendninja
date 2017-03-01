ec11054b11464107d0d68af40576ff51380341be
Lammerlord92 <javcarfer92@gmail.com>
2017-03-01 19:37:48 +0100

    Realizando validación de formularios

--- src/main/java/com/udemy/controllers/Example3Controller.java
+++ src/main/java/com/udemy/controllers/Example3Controller.java
@@ -1,9 +1,12 @@
 package com.udemy.controllers;
 
+import javax.validation.Valid;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,20 +39,20 @@ public class Example3Controller {
 	
 	@GetMapping("/showform")
 	public String showForm(Model model){
-//		LOGGER.info("INFO TRACE");
-//		LOGGER.warn("WARNING TRACE");
-//		LOGGER.error("ERROR TRACE");
-//		LOGGER.debug("DEBUG TRACE");
 		model.addAttribute("person", new Person());
 		return FORM_VIEW;
 	}
 	
-	@PostMapping("/addperson")
-	public ModelAndView addPerson(@ModelAttribute("person") Person person){
-		LOGGER.info("METHOD 'adPerson' -- PARAMS: '"+person+"'");
-		ModelAndView result=new ModelAndView(RESULT_VIEW);
-		result.addObject("person", person);
-		LOGGER.info("TEMPLATE: '"+RESULT_VIEW+"' -- DATA: '"+person+"'");
+	@PostMapping("/addperson") //BindingResult=>Resultado que da spring al validar el formulario, según las anotaciones, requiere @Valid
+	public ModelAndView addPerson(@Valid @ModelAttribute("person") Person person, BindingResult bindingResult){
+		//Si hay errores en el formulario (ver anotaciones del dominio)
+		ModelAndView result=new ModelAndView();
+		if(bindingResult.hasErrors()){
+			result.setViewName(FORM_VIEW);
+		}else{
+			result.setViewName(RESULT_VIEW);
+			result.addObject("person", person);
+		}
 		return result;
 	}
 
--- src/main/java/com/udemy/domain/Person.java
+++ src/main/java/com/udemy/domain/Person.java
@@ -1,7 +1,15 @@
 package com.udemy.domain;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class Person {
+	@NotNull
+	@Size(min=2,max=6)
 	private String name;
+	@NotNull
+	@Min(18)
 	private int age;
 		
 	public Person() {
